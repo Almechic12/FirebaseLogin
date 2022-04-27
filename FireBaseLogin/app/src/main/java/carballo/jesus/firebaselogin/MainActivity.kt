77773de27bf6d00253b79b167a3dec4f2ed18d8f
Almechic12 <ediithcarbherre25@gmail.com>
2022-04-27 11:52:06 -0700

    login

---
+++ FireBaseLogin/app/src/main/java/carballo/jesus/firebaselogin/MainActivity.kt
@@ -0,0 +1,28 @@
+package carballo.jesus.firebaselogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import carballo.jesus.firebaselogin.databinding.ActivityMainBinding
+import carballo.jesus.firebaselogin.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding= ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Initialize Firebase Auth
+        auth= Firebase.auth
+    }
+
+
+
+
+}
